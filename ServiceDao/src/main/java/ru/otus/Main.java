5c38616e88af37be3905956c84ed052119a51e0a
AlexandrYurkov <72255116+AlexandrYurkov@users.noreply.github.com>
2024-11-14 12:46:30 +0300

    Revert "dz"

--- ServiceDao/src/main/java/ru/otus/Main.java
+++ ServiceDao/src/main/java/ru/otus/Main.java
@@ -1,17 +1,7 @@
 package ru.otus;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
-
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-
-        Service service = new ItemsServiceProxy();
-        service.run();
-        service.UpdateTable();
-        service.ClosetedConnection();
+    public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
--- ServiceDao/src/main/java/ru/otus/Service.java
+++
@@ -1,9 +0,0 @@
-package ru.otus;
-
-import java.sql.SQLException;
-
-public interface Service {
-    public void run() throws SQLException;
-    public void UpdateTable() throws SQLException;
-    public void ClosetedConnection() throws SQLException;
-}
